1af40f89853aeb08f0305d759c1e105b2bb5bbc8
skybluejaecho <skybluejaecho@gmail.com>
2026-09-03 17:40:56 +0900

    feat(ingress): 요청 출처와 멱등성 범위 추가

--- src/jb_orchestrator/domain/__init__.py
+++ src/jb_orchestrator/domain/__init__.py
@@ -3,6 +3,7 @@
 from jb_orchestrator.domain.dispatches import RequestDispatchReceipt
 from jb_orchestrator.domain.events import DomainEvent
 from jb_orchestrator.domain.exceptions import DomainValidationError, InvalidStateTransition
+from jb_orchestrator.domain.ingress import RequestOrigin
 from jb_orchestrator.domain.projects import Project, ProjectStatus
 from jb_orchestrator.domain.requests import RequestStatus, UserRequest
 from jb_orchestrator.domain.runs import Run, RunStatus
@@ -14,6 +15,7 @@ __all__ = [
     "Project",
     "ProjectStatus",
     "RequestDispatchReceipt",
+    "RequestOrigin",
     "RequestStatus",
     "Run",
     "RunStatus",
